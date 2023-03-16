be0e6111fc05bdec43828ac8818dc70f436b8326
ArkhipovG <116101092+ArkhipovG@users.noreply.github.com>
2023-03-16 18:56:28 +0200

    FirstCommit

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Григорий Архипов on 16/03/2023.
+//
+
+import Foundation
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var bioTextView: UITextView!
+
+}
